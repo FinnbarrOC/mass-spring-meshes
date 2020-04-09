c78d259a8e30f485d0d11baef7c1a17affcaf21c
Finnbarr O'Callahan <lukasocallahan@gmail.com>
2020-04-09 18:31:03 -0400

    Added new packages and dropping ball for testing

---
+++ Assets/Scripts/SpawnerOnMouse.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerOnMouse : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+    public float heightOffset;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity))
+            {
+                Vector3 spawnPosition = hit.point;
+                spawnPosition.y += heightOffset;
+                
+                Instantiate(objectToSpawn, spawnPosition, Quaternion.identity);
+            }
+        }
+    }
+}
