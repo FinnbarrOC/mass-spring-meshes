880c1c62af0be17c13cf0ba6be7c9e4dbb9ab554
Finnbarr O'Callahan <lukasocallahan@gmail.com>
2020-06-10 13:27:58 -0400

    Added Mirror and UnityStandardAssets

---
+++ Assets/Scripts/ProjectileShooter.cs
@@ -0,0 +1,37 @@
+﻿using Mirror;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class ProjectileShooter : NetworkBehaviour
+{
+    [Header("Firing")]
+    public KeyCode shootKey = KeyCode.Space;
+    public GameObject projectilePrefab;
+    public Transform projectileMount;
+
+    void Update()
+    {
+        // shoot
+        if (Input.GetKeyDown(shootKey))
+        {
+            CmdFire();
+        }
+    }
+
+    // this is called on the server
+    [Command]
+    void CmdFire()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, projectileMount.position, transform.rotation);
+        NetworkServer.Spawn(projectile);
+        RpcOnFire();
+    }
+
+    // this is called on the tank that fired for all observers
+    [ClientRpc]
+    void RpcOnFire()
+    {
+        // TODO: play shooting animation or something
+    }
+}
