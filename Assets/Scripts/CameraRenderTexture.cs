36b3b7ba9289e241edb344e9c56c769dac88b8d7
Finnbarr O'Callahan <lukasocallahan@gmail.com>
2020-07-15 21:03:42 -0400

    Added some invisiblity and triangulation shaders

---
+++ Assets/Scripts/CameraRenderTexture.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraRenderTexture : MonoBehaviour
+{
+    [SerializeField] private Material material;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
